1e1035d0ac172d096bee139a21369cf256774297
Wayne_Mac <wayne65@gmail.com>
2022-10-20 19:19:18 +0800

    feat: add runners chart

--- main.js
+++ main.js
@@ -7,30 +7,36 @@ $(function () {
 		let qualityLineColor = '';
 		let shoesQuantity;
 		let shoesFloor;
+		let runnersQuantity;
 		switch (_shoesType) {
 			case 'common':
-				strShoesType = '灰鞋';
+				strShoesType = '灰';
 				qualityLineColor = 'rgb(169, 169, 169)';
 				shoesQuantity = _data.aryCommonShoesQuantity;
 				shoesFloor = _data.aryCommonShoesFloor;
 				break;
 			case 'uncommon':
-				strShoesType = '綠鞋';
+				strShoesType = '綠';
 				qualityLineColor = 'rgb(34, 139, 34)';
 				shoesQuantity = _data.aryUncommonShoesQuantity;
 				shoesFloor = _data.aryUncommonShoesFloor;
 				break;
 			case 'rare':
-				strShoesType = '藍鞋';
+				strShoesType = '藍';
 				qualityLineColor = 'rgb(0, 191, 255)';
 				shoesQuantity = _data.aryRareShoesQuantity;
 				shoesFloor = _data.aryRareShoesFloor;
 				break;
 			case 'epic':
-				strShoesType = '紫鞋';
+				strShoesType = '紫';
 				qualityLineColor = 'rgb(148, 0, 211)';
 				shoesQuantity = _data.aryEpicShoesQuantity;
 				shoesFloor = _data.aryEpicShoesFloor;
+				break;
+			case 'runners':
+				strShoesType = '跑者人數';
+				qualityLineColor = 'rgb(255, 69, 0)';
+				runnersQuantity = _data.aryRunners;
 				break;
 
 			default:
@@ -39,30 +45,39 @@ $(function () {
 
 		let aryDataSets = [];
 		switch (_shoesType) {
+			case 'runners':
+				aryDataSets.push({
+					label: '跑者 - 數量',
+					yAxisID: 'IdCommonQuantity',
+					backgroundColor: qualityLineColor,
+					borderColor: qualityLineColor,
+					data: runnersQuantity,
+				});
+				break;
 			case '4_shoes_quantity':
 				aryDataSets.push({
-					label: '灰鞋',
+					label: '灰',
 					yAxisID: 'IdCommonQuantity',
 					backgroundColor: 'rgb(169, 169, 169)',
 					borderColor: 'rgb(169, 169, 169)',
 					data: _data.aryCommonShoesQuantity,
 				});
 				aryDataSets.push({
-					label: '綠鞋',
+					label: '綠',
 					yAxisID: 'IdCommonQuantity',
 					backgroundColor: 'rgb(34, 139, 34)',
 					borderColor: 'rgb(34, 139, 34)',
 					data: _data.aryUncommonShoesQuantity,
 				});
 				aryDataSets.push({
-					label: '藍鞋',
+					label: '藍',
 					yAxisID: 'IdCommonQuantity',
 					backgroundColor: 'rgb(0, 191, 255)',
 					borderColor: 'rgb(0, 191, 255)',
 					data: _data.aryRareShoesQuantity,
 				});
 				aryDataSets.push({
-					label: '紫鞋',
+					label: '紫',
 					yAxisID: 'IdCommonQuantity',
 					backgroundColor: 'rgb(148, 0, 211)',
 					borderColor: 'rgb(148, 0, 211)',
@@ -72,28 +87,28 @@ $(function () {
 
 			case '4_shoes_floor':
 				aryDataSets.push({
-					label: '灰鞋',
+					label: '灰',
 					yAxisID: 'IdCommonFloor',
 					backgroundColor: 'rgb(169, 169, 169)',
 					borderColor: 'rgb(169, 169, 169)',
 					data: _data.aryCommonShoesFloor,
 				});
 				aryDataSets.push({
-					label: '綠鞋',
+					label: '綠',
 					yAxisID: 'IdCommonFloor',
 					backgroundColor: 'rgb(34, 139, 34)',
 					borderColor: 'rgb(34, 139, 34)',
 					data: _data.aryUncommonShoesFloor,
 				});
 				aryDataSets.push({
-					label: '藍鞋',
+					label: '藍',
 					yAxisID: 'IdCommonFloor',
 					backgroundColor: 'rgb(0, 191, 255)',
 					borderColor: 'rgb(0, 191, 255)',
 					data: _data.aryRareShoesFloor,
 				});
 				aryDataSets.push({
-					label: '紫鞋',
+					label: '紫',
 					yAxisID: 'IdCommonFloor',
 					backgroundColor: 'rgb(148, 0, 211)',
 					borderColor: 'rgb(148, 0, 211)',
@@ -101,7 +116,7 @@ $(function () {
 				});
 				break;
 
-			default:		
+			default:
 				aryDataSets.push({
 					label: `${strShoesType} - 數量`,
 					yAxisID: 'IdCommonQuantity',
@@ -269,19 +284,31 @@ $(function () {
 	});
 
 	$('#btn-4-quantity').on('click', function (e) {
-		stepnChart.data.datasets = prepareDataSets(stepnData, '4_shoes_quantity');
+		stepnChart.data.datasets = prepareDataSets(
+			stepnData,
+			'4_shoes_quantity'
+		);
 		stepnChart.options.scales.IdCommonFloor.display = false;
 		stepnChart.options.scales.IdCommonFloor.position = 'right';
 		stepnChart.options.scales.IdCommonQuantity.display = true;
 		stepnChart.resetZoom();
 		stepnChart.update();
 	});
-	
+
 	$('#btn-4-floor').on('click', function (e) {
 		stepnChart.data.datasets = prepareDataSets(stepnData, '4_shoes_floor');
 		stepnChart.options.scales.IdCommonFloor.display = true;
 		stepnChart.options.scales.IdCommonFloor.position = 'left';
 		stepnChart.options.scales.IdCommonQuantity.display = false;
+		stepnChart.resetZoom();
+		stepnChart.update();
+	});
+
+	$('#btn-runners').on('click', function (e) {
+		stepnChart.data.datasets = prepareDataSets(stepnData, 'runners');
+		stepnChart.options.scales.IdCommonFloor.display = false;
+		stepnChart.options.scales.IdCommonFloor.position = 'right';
+		stepnChart.options.scales.IdCommonQuantity.display = true;
 		stepnChart.resetZoom();
 		stepnChart.update();
 	});
